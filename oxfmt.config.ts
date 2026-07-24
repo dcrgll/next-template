13b2e8df55c3c2ff14f7a30eb6af0cf11fc50acf
Dan Cargill <dan@cargill.dev>
2026-07-24 22:01:27 +0100

    docs: reflow SECURITY.md paragraphs

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -3,7 +3,6 @@ import ultracite from 'ultracite/oxfmt'
 
 export default defineConfig({
 	...ultracite,
-	ignorePatterns: [...(ultracite.ignorePatterns ?? []), 'SECURITY.md'],
 	jsDoc: true,
 	jsxSingleQuote: true,
 	printWidth: 80,
