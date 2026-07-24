f4a1c4ed4f60b61e675dfc254b8421d9743e6fec
Dan Cargill <dan@cargill.dev>
2026-07-24 20:36:03 +0100

    chore: ignore SECURITY.md in oxfmt

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -3,6 +3,7 @@ import ultracite from 'ultracite/oxfmt'
 
 export default defineConfig({
 	...ultracite,
+	ignorePatterns: [...(ultracite.ignorePatterns ?? []), 'SECURITY.md'],
 	jsDoc: true,
 	jsxSingleQuote: true,
 	printWidth: 80,
